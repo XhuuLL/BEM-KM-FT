1ffb383206625bc4144f4fa12f58cd46e5ff9a0b
XhuuLL <fatkhularif15@gmail.com>
2026-01-25 21:42:05 +0700

    fix: force build by ignoring linting and ts errors

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,14 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
     domains: ["picsum.photos"],
-    // Tambahkan ini agar tidak error saat deploy jika ada warning gambar
     unoptimized: true, 
   },
-  // --- TAMBAHKAN BAGIAN DI BAWAH INI ---
   eslint: {
-    // Ini akan mengabaikan error linting (seperti variabel tak terpakai) saat build
+    // Mengabaikan error linting (variabel tak terpakai, dll)
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ini akan mengabaikan error tipe data saat build
+    // Mengabaikan error tipe data
     ignoreBuildErrors: true,
   },
 };
